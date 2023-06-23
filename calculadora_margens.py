84875a94a28b459116ba30d0940257ea3cb6df43
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-23 06:36:55 -0300

    calculadora_margens.py

--- calculadora_margens.py
+++ calculadora_margens.py
@@ -1,25 +1,34 @@
-def calcular_probabilidade_impli(custo, preco):
-    probabilidade_impli = (preco - custo) / preco * 100
-    return probabilidade_impli
+import streamlit as st
 
-def calcular_margem(custo, preco):
-    margem = (preco - custo) / preco * 100
+def calcular_margem(odd_casa, odd_empate, odd_visitante):
+    possibilidade_casa = 1 / odd_casa
+    possibilidade_empate = 1 / odd_empate
+    possibilidade_visitante = 1 / odd_visitante
+    margem = (possibilidade_casa + possibilidade_empate + possibilidade_visitante - 1) * 100
     return margem
 
-def calcular_margem_lucro(custo, preco):
-    margem_lucro = (preco - custo) / custo * 100
-    return margem_lucro
+def calcular_comissao(odd_casa, odd_empate, odd_visitante):
+    possibilidade_casa = 1 / odd_casa
+    possibilidade_empate = 1 / odd_empate
+    possibilidade_visitante = 1 / odd_visitante
+    comissao_casa = (1 / possibilidade_casa - 1) * 100
+    comissao_empate = (1 / possibilidade_empate - 1) * 100
+    comissao_visitante = (1 / possibilidade_visitante - 1) * 100
+    return comissao_casa, comissao_empate, comissao_visitante
 
-# Solicitar os valores de custo e preço
-custo = float(input("Digite o custo do produto: "))
-preco = float(input("Digite o preço de venda do produto: "))
+# Interface do Streamlit
+st.title("Calculadora de Margens")
 
-# Chame as funções e imprima os resultados
-probabilidade_impli = calcular_probabilidade_impli(custo, preco)
-margem = calcular_margem(custo, preco)
-margem_lucro = calcular_margem_lucro(custo, preco)
+odd_casa = st.number_input("Odd Casa")
+odd_empate = st.number_input("Odd Empate")
+odd_visitante = st.number_input("Odd Visitante")
 
-print("Probabilidade Implícita: ", probabilidade_impli)
-print("Margem: ", margem)
-print("Margem de Lucro: ", margem_lucro)
+if st.button("Calcular"):
+    margem = calcular_margem(odd_casa, odd_empate, odd_visitante)
+    comissao_casa, comissao_empate, comissao_visitante = calcular_comissao(odd_casa, odd_empate, odd_visitante)
+
+    st.write("Margem: {:.2f}%".format(margem))
+    st.write("Comissão para Casa: {:.2f}%".format(comissao_casa))
+    st.write("Comissão para Empate: {:.2f}%".format(comissao_empate))
+    st.write("Comissão para Visitante: {:.2f}%".format(comissao_visitante))
 
