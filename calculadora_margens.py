621c7f4fd3b7c725b582c59a8784dfc86c5d23a4
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-23 06:39:32 -0300

    calculadora_margens.py

--- calculadora_margens.py
+++ calculadora_margens.py
@@ -1,34 +1,29 @@
 import streamlit as st
 
-def calcular_margem(odd_casa, odd_empate, odd_visitante):
-    possibilidade_casa = 1 / odd_casa
-    possibilidade_empate = 1 / odd_empate
-    possibilidade_visitante = 1 / odd_visitante
-    margem = (possibilidade_casa + possibilidade_empate + possibilidade_visitante - 1) * 100
-    return margem
-
-def calcular_comissao(odd_casa, odd_empate, odd_visitante):
-    possibilidade_casa = 1 / odd_casa
-    possibilidade_empate = 1 / odd_empate
-    possibilidade_visitante = 1 / odd_visitante
-    comissao_casa = (1 / possibilidade_casa - 1) * 100
-    comissao_empate = (1 / possibilidade_empate - 1) * 100
-    comissao_visitante = (1 / possibilidade_visitante - 1) * 100
-    return comissao_casa, comissao_empate, comissao_visitante
+def calcular_odd_justa(odd, margem):
+    odd_justa = odd / (1 + margem/100)
+    return round(odd_justa, 2)
 
 # Interface do Streamlit
-st.title("Calculadora de Margens")
+st.title("Calculadora de Odds Justas")
 
 odd_casa = st.number_input("Odd Casa")
 odd_empate = st.number_input("Odd Empate")
 odd_visitante = st.number_input("Odd Visitante")
 
-if st.button("Calcular"):
-    margem = calcular_margem(odd_casa, odd_empate, odd_visitante)
-    comissao_casa, comissao_empate, comissao_visitante = calcular_comissao(odd_casa, odd_empate, odd_visitante)
+margem = st.slider("Margem da Casa de Apostas (%)", min_value=0.0, max_value=100.0, step=0.01)
 
-    st.write("Margem: {:.2f}%".format(margem))
-    st.write("Comissão para Casa: {:.2f}%".format(comissao_casa))
-    st.write("Comissão para Empate: {:.2f}%".format(comissao_empate))
-    st.write("Comissão para Visitante: {:.2f}%".format(comissao_visitante))
+if st.button("Calcular"):
+    odd_justa_casa = calcular_odd_justa(odd_casa, margem)
+    odd_justa_empate = calcular_odd_justa(odd_empate, margem)
+    odd_justa_visitante = calcular_odd_justa(odd_visitante, margem)
+
+    # Verificar se a soma dos resultados é maior que 100%
+    soma_odds_justas = odd_justa_casa + odd_justa_empate + odd_justa_visitante
+    if soma_odds_justas > 100.0:
+        st.warning("A soma das odds justas é maior que 100%. Ajuste as odds ou a margem.")
+
+    st.write("Odd Justa Casa: {:.2f}".format(odd_justa_casa))
+    st.write("Odd Justa Empate: {:.2f}".format(odd_justa_empate))
+    st.write("Odd Justa Visitante: {:.2f}".format(odd_justa_visitante))
 
