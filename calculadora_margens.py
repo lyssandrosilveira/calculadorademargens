fc9e6c6eed5ae9d4492cc69d9bdd6e21fda6fd93
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-23 07:12:59 -0300

    Update calculadora_margens.py

--- calculadora_margens.py
+++ calculadora_margens.py
@@ -14,7 +14,9 @@ def calcular_probabilidades(odd_casa, odd_empate, odd_visitante):
     odds_justa_odd_empate = 100 / prob_justa_odd_empate
     odds_justa_odd_visitante = 100 / prob_justa_odd_visitante
 
-    return prob_justa_odd_casa, prob_justa_odd_empate, prob_justa_odd_visitante
+    juice = 100 - (prob_justa_odd_casa + prob_justa_odd_empate + prob_justa_odd_visitante)
+
+    return prob_justa_odd_casa, prob_justa_odd_empate, prob_justa_odd_visitante, juice
 
 # Interface do Streamlit
 st.title("Qual a Probabilidade Real?")
@@ -24,8 +26,9 @@ odd_empate = st.number_input("Odd Empate")
 odd_visitante = st.number_input("Odd Visitante")
 
 if st.button("Calcular"):
-    prob_casa, prob_empate, prob_visitante = calcular_probabilidades(odd_casa, odd_empate, odd_visitante)
+    prob_casa, prob_empate, prob_visitante, juice = calcular_probabilidades(odd_casa, odd_empate, odd_visitante)
 
     st.write("Probabilidade Real Casa: {:.2f}%".format(prob_casa))
     st.write("Probabilidade Real Empate: {:.2f}%".format(prob_empate))
     st.write("Probabilidade Real Visitante: {:.2f}%".format(prob_visitante))
+    st.write("Juice da Casa de Apostas: {:.2f}%".format(juice))
