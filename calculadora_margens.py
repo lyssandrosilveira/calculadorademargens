15288c5243b071ddeea027d2257d38dab25698ef
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-23 07:11:07 -0300

    Update calculadora_margens.py

--- calculadora_margens.py
+++ calculadora_margens.py
@@ -17,7 +17,7 @@ def calcular_probabilidades(odd_casa, odd_empate, odd_visitante):
     return prob_justa_odd_casa, prob_justa_odd_empate, prob_justa_odd_visitante
 
 # Interface do Streamlit
-st.title("Calculadora de Probabilidade Real")
+st.title("Qual a Probabilidade Real?")
 
 odd_casa = st.number_input("Odd Casa")
 odd_empate = st.number_input("Odd Empate")
