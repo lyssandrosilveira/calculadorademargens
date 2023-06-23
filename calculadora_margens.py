e86b47e0f0346844f5c46477959718edf27c8859
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-23 07:17:41 -0300

    Update calculadora_margens.py

--- calculadora_margens.py
+++ calculadora_margens.py
@@ -1,15 +1,20 @@
 import streamlit as st
 
 def calcular_probabilidades(odd_casa, odd_empate, odd_visitante):
-    total_odds = odd_casa + odd_empate + odd_visitante
+    prob_odd_casa = 1 / odd_casa
+    prob_odd_empate = 1 / odd_empate
+    prob_odd_visitante = 1 / odd_visitante
+    total_prob = prob_odd_casa + prob_odd_empate + prob_odd_visitante
 
-    prob_odd_casa = odd_casa / total_odds * 100
-    prob_odd_empate = odd_empate / total_odds * 100
-    prob_odd_visitante = odd_visitante / total_odds * 100
+    prob_justa_odd_casa = prob_odd_casa / total_prob * 100
+    prob_justa_odd_empate = prob_odd_empate / total_prob * 100
+    prob_justa_odd_visitante = prob_odd_visitante / total_prob * 100
 
-    juice = 100 - (prob_odd_casa + prob_odd_empate + prob_odd_visitante)
+    odds_justa_odd_casa = 100 / prob_justa_odd_casa
+    odds_justa_odd_empate = 100 / prob_justa_odd_empate
+    odds_justa_odd_visitante = 100 / prob_justa_odd_visitante
 
-    return prob_odd_casa, prob_odd_empate, prob_odd_visitante, juice
+    return prob_justa_odd_casa, prob_justa_odd_empate, prob_justa_odd_visitante
 
 # Interface do Streamlit
 st.title("Qual a Probabilidade Real?")
@@ -19,9 +24,8 @@ odd_empate = st.number_input("Odd Empate")
 odd_visitante = st.number_input("Odd Visitante")
 
 if st.button("Calcular"):
-    prob_casa, prob_empate, prob_visitante, juice = calcular_probabilidades(odd_casa, odd_empate, odd_visitante)
+    prob_casa, prob_empate, prob_visitante = calcular_probabilidades(odd_casa, odd_empate, odd_visitante)
 
     st.write("Probabilidade Real Casa: {:.2f}%".format(prob_casa))
     st.write("Probabilidade Real Empate: {:.2f}%".format(prob_empate))
     st.write("Probabilidade Real Visitante: {:.2f}%".format(prob_visitante))
-    st.write("Juice da Casa de Apostas: {:.2f}%".format(juice))
