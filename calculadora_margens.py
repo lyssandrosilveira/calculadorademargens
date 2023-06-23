8c3d1c37f0d8880d2e4f0d44146b7d5cb52f9793
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-23 06:54:18 -0300

    calculadora_margens.py

--- calculadora_margens.py
+++ calculadora_margens.py
@@ -20,12 +20,21 @@ if st.button("Calcular"):
         st.warning("A soma das odds é maior que 100%. Ajuste as odds.")
 
     # Calcular as probabilidades reais
-    probabilidade_real_casa = calcular_probabilidade_real(odd_casa / (1 + margem))
-    probabilidade_real_empate = calcular_probabilidade_real(odd_empate / (1 + margem))
-    probabilidade_real_visitante = calcular_probabilidade_real(odd_visitante / (1 + margem))
+    probabilidade_real_casa = calcular_probabilidade_real(1 / odd_casa)
+    probabilidade_real_empate = calcular_probabilidade_real(1 / odd_empate)
+    probabilidade_real_visitante = calcular_probabilidade_real(1 / odd_visitante)
+
+    # Calcular as odds justas subtraindo o juice
+    odd_justa_casa = odd_casa / (1 + margem)
+    odd_justa_empate = odd_empate / (1 + margem)
+    odd_justa_visitante = odd_visitante / (1 + margem)
 
     st.write("Juice (Margem da Casa de Apostas): {:.2f}%".format(margem * 100))
     st.write("Probabilidade Real Casa: {:.2f}%".format(probabilidade_real_casa))
     st.write("Probabilidade Real Empate: {:.2f}%".format(probabilidade_real_empate))
     st.write("Probabilidade Real Visitante: {:.2f}%".format(probabilidade_real_visitante))
+    st.write("Odd Justa Casa: {:.2f}".format(odd_justa_casa))
+    st.write("Odd Justa Empate: {:.2f}".format(odd_justa_empate))
+    st.write("Odd Justa Visitante: {:.2f}".format(odd_justa_visitante))
+
 
