3b3d3c9936b796d7791521eac4dc804dc00c9033
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-23 06:50:44 -0300

    calculadora_margens.py

--- calculadora_margens.py
+++ calculadora_margens.py
@@ -1,34 +1,31 @@
 import streamlit as st
 
-def calcular_margem(odd_casa, odd_empate, odd_visitante):
-    margem = 1 / odd_casa + 1 / odd_empate + 1 / odd_visitante - 1
-    return round(margem * 100, 2)
-
-def calcular_odd_justa(odd, margem):
-    odd_justa = odd / (1 + margem/100)
-    return round(odd_justa, 2)
+def calcular_probabilidade_real(odd):
+    probabilidade_real = 1 / odd * 100
+    return round(probabilidade_real, 2)
 
 # Interface do Streamlit
-st.title("Qual Odd Justa?")
+st.title("Calculadora de Probabilidade Real")
 
 odd_casa = st.number_input("Odd Casa")
 odd_empate = st.number_input("Odd Empate")
 odd_visitante = st.number_input("Odd Visitante")
 
 if st.button("Calcular"):
-    margem = calcular_margem(odd_casa, odd_empate, odd_visitante)
-
-    odd_justa_casa = calcular_odd_justa(odd_casa, margem)
-    odd_justa_empate = calcular_odd_justa(odd_empate, margem)
-    odd_justa_visitante = calcular_odd_justa(odd_visitante, margem)
+    # Calcular a margem da casa de apostas (juice)
+    margem = 1 / odd_casa + 1 / odd_empate + 1 / odd_visitante - 1
 
     # Verificar se a soma dos resultados é maior que 100%
-    soma_odds_justas = odd_justa_casa + odd_justa_empate + odd_justa_visitante
-    if soma_odds_justas > 100.0:
-        st.warning("A soma das odds justas é maior que 100%. Ajuste as odds.")
+    if margem > 1:
+        st.warning("A soma das odds é maior que 100%. Ajuste as odds.")
 
-    st.write("Margem da Casa de Apostas (Juice): {:.2f}%".format(margem))
-    st.write("Odd Justa Casa: {:.2f}".format(odd_justa_casa))
-    st.write("Odd Justa Empate: {:.2f}".format(odd_justa_empate))
-    st.write("Odd Justa Visitante: {:.2f}".format(odd_justa_visitante))
+    # Calcular as probabilidades reais
+    probabilidade_real_casa = calcular_probabilidade_real(odd_casa / (1 + margem))
+    probabilidade_real_empate = calcular_probabilidade_real(odd_empate / (1 + margem))
+    probabilidade_real_visitante = calcular_probabilidade_real(odd_visitante / (1 + margem))
+
+    st.write("Juice (Margem da Casa de Apostas): {:.2f}%".format(margem * 100))
+    st.write("Probabilidade Real Casa: {:.2f}%".format(probabilidade_real_casa))
+    st.write("Probabilidade Real Empate: {:.2f}%".format(probabilidade_real_empate))
+    st.write("Probabilidade Real Visitante: {:.2f}%".format(probabilidade_real_visitante))
 
